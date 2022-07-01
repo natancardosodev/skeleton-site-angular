84f53dbe82ee680b876ce766726d8cf9127e2c95
Natan Cardoso <natan@voxtecnologia.com.br>
2022-06-30 22:30:02 -0300

    fix name project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/skeleton-site-angular'),
+      dir: require('path').join(__dirname, './coverage/skeleton-angular-basic'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'skeleton-site-angular'`, () => {
+  it(`should have as title 'skeleton-angular-basic'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('skeleton-site-angular');
+    expect(app.title).toEqual('skeleton-angular-basic');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('skeleton-site-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('skeleton-angular-basic app is running!');
   });
 });
